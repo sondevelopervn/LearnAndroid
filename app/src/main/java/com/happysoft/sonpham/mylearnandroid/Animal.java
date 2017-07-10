80f86078d6e6607b599e26cdc5020dadfcfa36b0
DESKTOP-LI1TKTF\COMPAQ <hungson12007@gmail.com>
2017-07-10 23:58:07 +0700

    khoi tao du an

--- app/src/main/java/com/happysoft/sonpham/mylearnandroid/Animal.java
+++
@@ -1,9 +0,0 @@
-package com.happysoft.sonpham.mylearnandroid;
-
-/**
- * Created by COMPAQ on 14/04/2017.
- */
-
-public interface Animal {
-     void say_hello();
-}
--- app/src/main/java/com/happysoft/sonpham/mylearnandroid/MainActivity.java
+++ app/src/main/java/com/happysoft/sonpham/mylearnandroid/MainActivity.java
@@ -7,21 +7,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView mShowdata;
+    TextView mShowData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowdata= (TextView) findViewById(R.id.textView2);
-
-        Dog dog=new Dog();
-        dog.show();
-        Cat cat=new Cat();
-        cat.show();
-
-        Bike obj = new Honda4();
-        obj.run();
+        mShowData= (TextView) findViewById(R.id.textView2);
 
 
 
